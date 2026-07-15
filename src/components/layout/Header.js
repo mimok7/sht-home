46d02c4e4cf739a265fc3500038a31e65f7209d8
Lee Eun-mi <candy386@goedu.kr>
2026-07-15 13:20:17 +0900

    07-14-1320

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,6 @@
 'use client';
 import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname } from 'next/navigation';
 import './Header.css';
-export default function Header(){const temporary = usePathname() === '/temp-home'; return <header className="header glass"><div className="container header-content">{temporary ? <div className="logo"><span className="logo-mark">SH</span><span>STAY <b>HALONG</b><small>CURATED BAY JOURNEYS</small></span></div> : <Link href="/" className="logo"><span className="logo-mark">SH</span><span>STAY <b>HALONG</b><small>CURATED BAY JOURNEYS</small></span></Link>}</div></header>}
+export default function Header(){const temporary = usePathname() === '/temp-home'; return <header className="header glass"><div className="container header-content">{temporary ? <div className="logo"><Image className="logo-mark" src="/images/cruises/logo2.png" alt="SH" width={187} height={183} /><span>STAY <b>HALONG</b><small>CURATED BAY JOURNEYS</small></span></div> : <Link href="/" className="logo"><Image className="logo-mark" src="/images/cruises/logo2.png" alt="SH" width={187} height={183} /><span>STAY <b>HALONG</b><small>CURATED BAY JOURNEYS</small></span></Link>}</div></header>}

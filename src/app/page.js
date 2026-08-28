a9547b52ee2c98bb80156848bbe4f1e43b64f8a2
Lee Eun-mi <candy386@goedu.kr>
2026-08-29 08:59:03 +0900

    Move main homepage to home route

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
-import TemporaryHome from './temp-home/page';
+import { permanentRedirect } from 'next/navigation';
 
-export const metadata = { title: 'STAY HALONG | 스테이하롱 - 하롱베이 현지 프리미엄 여행사' };
-export const revalidate = 300;
-
-export default TemporaryHome;
+export default function RootPage() {
+  permanentRedirect('/home');
+}
